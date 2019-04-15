11df7b6019837b93b88f13e997b833d3dd223ca2
Michael Koloberdin <koloberdin@gmail.com>
2019-04-15 23:45:04 +0300

    Update a regression test to use correct DEFARRAY syntax

--- regression/listing/define.asm
+++ regression/listing/define.asm
@@ -3,10 +3,9 @@
         org #8000
         
         DEFINE Scalar db 1
-        DEFARRAY Array,db 2,db 3,db 4
+        DEFARRAY Array db 2,db 3,db 4
         
         Scalar
-        Array[3]
         Array[2]
         Array[1]
         Array[0]
